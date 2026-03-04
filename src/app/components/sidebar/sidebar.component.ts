3b8810800b742ed4f604fa47cf18e465f23648f4
JessicaAvila77 <jessica.avilaunah@gmail.com>
2026-03-03 21:08:58 -0600

    Mejorando dashboard

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+
+  // Controla qué menú está abierto
+  openMenu: string | null = null;
+
+  // Método para abrir/cerrar submenús
+  toggleMenu(menu: string): void {
+    this.openMenu = this.openMenu === menu ? null : menu;
+  }
 
 }
